bc71435e8c9f429b7060b2f2e9e1d4889523f852
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-01-22 20:11:21 +0530

    Create basicsyntax.java

---
+++ Arrays/basicsyntax.java
@@ -0,0 +1,12 @@
+public class basicsyntax {
+    public static void main(String[] args) {
+
+        int[] arr=new int[5];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=23;
+        arr[3]=26;
+        System.out.println(arr[1]);
+
+    }
+}
