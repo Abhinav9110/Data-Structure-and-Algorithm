e53e4ee8031321340d1faf80897566015986958d
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-02-06 11:23:25 +0530

    Add files via upload

---
+++ basicsyntax.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class basicsyntax {
+    public static void main(String[] args) {
+
+        int[] arr=new int[5];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=23;
+        arr[3]=26;
+        System.out.println(arr[1]);
+
+    }
+}
