c87c0d1418ba491aa8fbae9cbff3b9b386e51872
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-02-17 21:01:11 +0530

    Add files via upload

---
+++ Arrays/TumSum.java
@@ -0,0 +1,16 @@
+package Arrays;
+//WAP to find the doublet in the array whose sum is equal to given value of x.
+public class TumSum {
+    public static void main(String[] args) {
+        int [] arr={3,0,8,5,4,9,2};
+        int x=9;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(arr[i]+arr[j]==x){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+}
