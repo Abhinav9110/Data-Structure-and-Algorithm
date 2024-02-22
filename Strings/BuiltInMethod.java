23075042461ed114c262ff6c3c966d88f50646af
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-02-22 10:24:12 +0530

    Create BuiltInMethod.java

---
+++ Strings/BuiltInMethod.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class BuiltInMethod {
+    public static void main(String[] args) {
+        String s="Abhinav Yadav";
+        System.out.println(s);
+        int n=s.length();
+        System.out.println(n);
+        System.out.println(s.charAt(4));
+    }
+}
