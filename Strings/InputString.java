666515befb25800718d6263ef5647b9f297290bc
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-02-24 09:36:29 +0530

    Add files via upload

---
+++ Strings/InputString.java
@@ -0,0 +1,10 @@
+package Strings;
+import java.util.Scanner;
+public class InputString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Hi,Enter your Name:");
+        String str=sc.nextLine();
+        System.out.println(str);
+    }
+}
