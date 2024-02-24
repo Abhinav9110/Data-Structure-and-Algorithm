8a1cb009fcc69760f2f60e47b1571bb365fd2c0a
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-02-24 09:36:02 +0530

    Add files via upload

---
+++ Strings/IndexOf.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class IndexOf {
+    public static void main(String[] args) {
+       String s="Abhinav kumar";
+        System.out.println(s.indexOf('t'));
+        System.out.println(s.lastIndexOf('a'));
+        String b="Animesh Kumar";
+        System.out.println(s.compareTo(b));
+    }
+}
