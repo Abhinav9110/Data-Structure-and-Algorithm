2e7895a09eaf27a1e52bfc1909fc53233e9de29f
Abhinav Kumar <116913096+Abhinav9110@users.noreply.github.com>
2024-02-25 10:15:00 +0530

    Add files via upload

---
+++ Strings/plusOperator.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class plusOperator {
+    public static void main(String[] args) {
+        String s="abc";
+        System.out.println(10+20+"abc");
+        System.out.println("abc"+10+20);
+    }
+}
